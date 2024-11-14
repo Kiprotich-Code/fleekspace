a08b6bad14bf57802016dd72127fdd9833bb62ae
Kiprotich Code <fleeklawi@gmail.com>
2024-11-14 11:33:30 +0300

    blog + users complete api

--- api/blog/urls.py
+++ api/blog/urls.py
@@ -1,14 +1,14 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import CategoryViewSet, CommentViewSet, LikePostAPIView, PostViewSet
+from blog.views import CategoryViewSet, CommentViewSet, LikePostAPIView, PostViewSet
 
 app_name = "blog"
 
 router = DefaultRouter()
-router.register(r'categories', CategoryViewSet)
-router.register(r'^(?<post_id>\d+)/comment', CommentViewSet)
-router.register(r'', PostViewSet)
+router.register(r"categories", CategoryViewSet)
+router.register(r"^(?P<post_id>\d+)/comment/$", CommentViewSet)
+router.register(r"", PostViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
